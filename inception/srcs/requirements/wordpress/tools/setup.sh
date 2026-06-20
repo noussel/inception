372c45dd0e5947bec172519f76e68177c82efb21
nouss inception <noussaiba.elkhadar@usmba.ac.ma>
2026-06-20 08:52:46 +0100

    working on wordpress

--- inception/srcs/requirements/wordpress/tools/setup.sh
+++ inception/srcs/requirements/wordpress/tools/setup.sh
@@ -1,4 +1,4 @@
-# installer php-fpm
+# installer php-fpm (not in the script because it should be installed at the creation of the image)
 # installer les extensions PHP nécessaires
 # télécharger WordPress
 # créer wp-config.php
