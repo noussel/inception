4c5d1c2763c620bedfb43fee0d4e33feba3080f7
nel-khad <nel-khad@inceptionC4r2p3.myguest.virtualbox.org>
2026-07-09 16:10:49 +0100

    done

--- inception/srcs/requirements/wordpress/tools/setup.sh
+++ inception/srcs/requirements/wordpress/tools/setup.sh
@@ -44,12 +44,12 @@ rm -rf wordpress latest.tar.gz
         --title=$WP_TITLE \
         --admin_user=$WP_ADMIN_USER \
         --admin_password=$WP_ADMIN_PASSWORD \
-        --admin_email=$WP_ADMIN_EMAIL \
+        # --admin_email=$WP_ADMIN_EMAIL \
         --allow-root
 
     wp user create \
         $WP_USER \
-        $WP_USER_EMAIL \
+        # $WP_USER_EMAIL \
         --user_pass=$WP_USER_PASSWORD \
         --role=author \
         --allow-root
