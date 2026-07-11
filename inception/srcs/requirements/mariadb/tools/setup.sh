8085735b327df01282bccfbc23a74b6459fe8411
nel-khad <nel-khad@inceptionC4r2p3.myguest.virtualbox.org>
2026-07-11 14:57:41 +0100

    finishing

--- inception/srcs/requirements/mariadb/tools/setup.sh
+++ inception/srcs/requirements/mariadb/tools/setup.sh
@@ -6,45 +6,6 @@
 # 5. arrêter MariaDB
 # 6. lancer mysqld
 
-
-#!/bin/bash
-
-chown -R mysql:mysql /var/lib/mysql
-
-# # Initialisation (ghir awel mara)
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
-
-#     echo "[mariadb] Initializing database..."
-
-#     mysql_install_db --user=mysql --datadir=/var/lib/mysql
-# fi
-#     mysqld_safe &
-
-#     until mysqladmin ping -h 127.0.0.1 --silent; do
-#         echo "Waiting MariaDB..."
-#         sleep 1
-#     done
-
-#     mysql -u root -p"${MYSQL_ROOT_PASSWORD}"<< EOF
-#     CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
-#     CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-
-#     GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-
-#     ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-
-#     FLUSH PRIVILEGES;
-#     EOF
-
-#     mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
-#     wait $MYSQL_PID
-
-# # echo "[mariadb] Starting MariaDB..."
-
-# exec mysqld_safe
-
-
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "FIRST START"
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
